c81d137d4591c4a6d30d19840adc5db9f0c4bd9f
Vinicius Pontes Braga <viniciusbragacontatos@gmail.com>
2025-12-06 13:23:02 -0300

    Atualizando template base

---
+++ meuapp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('saudacao/<str:nome>/', views.saudacao, name='saudacao'),
+    path('produto/<int:id_produto>/', views.produtos, name='produtos'),
+    path('mensagem/', views.mensagem, name='mensagem'),
+    path('produtos/', views.produtos, name='produtos'),
+    path('index/', views.index, name='index')
+]
---
+++ meuapp/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import datetime
+
+def home(request):
+    nome = 'Vinicius'
+    return render(request, 'home.html', {'nome':nome})
+
+def mensagem(request):
+    nome = 'Vinicius'
+    horario = 7
+
+    if 5 <= horario < 12:
+        mensagem = f'Bom dia!'
+    elif 12 <= horario < 18:
+        mensagem = f'Boa tarde!'
+    else:
+        mensagem = f'Boa noite!'
+    return render(request, 'home.html', {'mensagem':mensagem, 'nome':nome})
+
+def saudacao(resquest, nome):
+    mensagem = f'Olá {nome}, seja bem vindx ao meu site!'
+    return HttpResponse(mensagem)
+
+"""def produtos(request, id_produto):
+    produtos = {
+        1: 'Notebook',
+        2: 'Mouse',
+        3: 'Fone'
+    }
+    produto = produtos.get(id_produto)
+    if produto is None:
+        mensagem = f'O produto não existe'
+        return HttpResponse(mensagem)
+    else:
+        mensagem = f'Detalhes do produto: {produto}'
+        return HttpResponse(mensagem)"""
+
+def produtos(request):
+    produtos = ['Notebook', 'Mouse', 'Fone', 'Mouse', 'Celular']
+    return render(request, 'produtos.html', {'produtos':produtos})
+
+def index(request):
+    ola = "Olá herança!"
+    return render(request, 'index.html', {'ola': ola})
